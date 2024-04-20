cc1b1365a827a1d0e8590eef04a7618cca896e7d
nandkishorr <rachulr7@gmail.com>
2024-04-21 00:40:06 +0530

    massdata model created

---
+++ src/models/massdata.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const mongooseDelete = require('mongoose-delete');
+const massDataSchema = new mongoose.Schema({
+    Name : {
+        type : String,
+        required : true
+    },
+    // Email : {
+    //     type : String,
+    //     required : true
+    // },
+    // Picture : {
+    //     type : String,
+    // },
+});
+
+userDataSchema.plugin(mongooseDelete, { overrideMethods: 'all' });
+const MassData = mongoose.model('userData', massDataSchema);
+
+module.exports = MassData;
